db5c5bf383a4f2c8f7c6cc08350565719bf6f439
L0nge <xyangclon@qq.com>
2017-09-27 18:12:30 +0800

    11

---
+++ src/main/java/com/my/test/test.java
@@ -0,0 +1,19 @@
+package com.my.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Longe on 2017.09.27.
+ */
+@Controller
+@RequestMapping(value = "/test")
+public class test {
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String hello(@RequestParam(value = "name")String name) {
+        return "hello" + name;
+    }
+}
