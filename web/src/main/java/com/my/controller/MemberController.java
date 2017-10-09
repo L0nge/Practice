516f4efcfb967f74b1d75539d6975093a2f8f267
L0nge <xyangclon@qq.com>
2017-10-09 17:20:54 +0800

    11

--- web/src/main/java/com/my/controller/MemberController.java
+++ web/src/main/java/com/my/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.my.controller;
 
 import com.my.entity.Member;
 import com.my.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "member")
 public class MemberController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MemberController.class);
+
     @Autowired
     private MemberService memberService;
 
@@ -21,6 +25,7 @@ public class MemberController {
     @ResponseBody
     public String getInfo(@RequestParam(value = "memMobile")String memMobile) {
         Member member = memberService.getMemberInfo(memMobile);
+        LOGGER.info(member.toString());
         return member.toString();
     }
 }
